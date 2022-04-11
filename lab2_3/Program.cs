7fa8a3073751365cb7583228e585c01f3bbe8033
VladyslavStashkevych <vladyslav.stashkevych.it.2021@lpnu.ua>
2022-04-11 18:09:56 +0300

    Add project files.

---
+++ lab2_3/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace lab2_3 {
+	internal static class Program {
+		static void Main(string[] args) {
+			VectorN s = new VectorN(3, new double[] { 1, 2, 3 });
+			Console.WriteLine("Vector s:");
+			s.Display();
+			Console.WriteLine("Vector s after changes (s + s):");
+			s = s + s;
+			s.Display();
+			Console.WriteLine($"Scalar (s, s): {s * s}\n");
+
+			VectorN v = new VectorN();
+			v.Read();
+			Console.WriteLine("Vector V:");
+			v.Display();
+			Console.WriteLine("Vector S:");
+			s.Display();
+			Console.WriteLine($"Scalar (s, v): {s * v}");
+			Console.WriteLine("Vector C = S - V");
+			VectorN res = s - v;
+			res.Display();
+			Console.WriteLine("Vector C = ++S - V--:");
+			res = ++s - v.PostDecrement();
+			res.Display();
+			Console.WriteLine("\nVector V:");
+			v.Display();
+			Console.WriteLine("Vector S:");
+			s.Display();
+		}
+	}
+}
